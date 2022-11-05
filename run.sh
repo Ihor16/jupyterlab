440358ea3613e5fc10fe58980abccb04ad61f3a2
Ihor16 <ihor.o.parkhomenko@gmail.com>
2022-11-05 02:40:55 -0700

    added quotes to remove word splitting

--- run.sh
+++ run.sh
@@ -6,20 +6,20 @@
 printf "\n=== Starting Jupyter... \n"
 docker compose up -d
 
-name=$(docker container ls --format {{.Names}} | grep jlab)
+name=$(docker container ls --format "{{.Names}}" | grep jlab)
 
 # waits till the link is available
-until [[ $(docker container logs $name 2>&1) == *"http:"* ]]; do
+until [[ $(docker container logs "$name" 2>&1) == *"http:"* ]]; do
     sleep 1
 done
 
 printf "\n=== Use the link below to access it:\n"
 
-link=$(docker container logs $name 2>&1 |
+link=$(docker container logs "$name" 2>&1 |
         grep "http://127.0.0.1" |
         grep "token" | 
         tail -n 1 | 
         awk '{print $2}')
-echo $link
+echo "$link"
 
 printf "\n=== Done ===\n"
